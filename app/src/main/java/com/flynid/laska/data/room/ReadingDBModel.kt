13340345fe8831565aa5f49058bb7514a0508cd6
ntnlbk <ntnlbk@gmail.com>
2026-03-26 18:55:40 +0100

    Room database implementation

---
+++ app/src/main/java/com/flynid/laska/data/room/ReadingDBModel.kt
@@ -0,0 +1,25 @@
+package com.flynid.laska.data.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "readings_table")
+data class ReadingDBModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val date: String,
+    val dateFormatted: String,
+    val language: String,
+    val bibleReference: String,
+    val bibleText: String,
+    val bibleTextPlain: String,
+    val feastName: String,
+    val reflectionTextFirst: String,
+    val reflectionTextSecond: String,
+    val authorName: String,
+    val audioURL: String,
+    val imageURL: String,
+    val audioLocalPath: String?,
+    val imageLocalPath: String?,
+    val permalink: String
+)
