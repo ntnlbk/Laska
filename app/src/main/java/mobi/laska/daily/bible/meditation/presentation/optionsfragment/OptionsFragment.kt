c67963a8c4c49584ba04c18b0bb813c4ab10893d
ntnlbk <ntnlbk@gmail.com>
2026-04-28 18:49:20 +0200

    Languages added

--- app/src/main/java/mobi/laska/daily/bible/meditation/presentation/optionsfragment/OptionsFragment.kt
+++ app/src/main/java/mobi/laska/daily/bible/meditation/presentation/optionsfragment/OptionsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -13,6 +14,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import mobi.laska.daily.bible.meditation.R
 import mobi.laska.daily.bible.meditation.databinding.FragmentOptionsBinding
 import mobi.laska.daily.bible.meditation.domain.Language
 import mobi.laska.daily.bible.meditation.domain.settings.Settings
@@ -56,8 +58,8 @@ class OptionsFragment : Fragment() {
                                     Language.BY -> "Беларуская"
                                 }
                                 binding.colorChosenTv.text = when (it.settings.textFragmentTheme) {
-                                    TextFragmentTheme.DARK -> "Тёмный"
-                                    TextFragmentTheme.LIGHT -> "Светлый"
+                                    TextFragmentTheme.DARK -> ContextCompat.getString(requireActivity(), R.string.dark)
+                                    TextFragmentTheme.LIGHT -> ContextCompat.getString(requireActivity(), R.string.light)
                                 }
                             }
 
